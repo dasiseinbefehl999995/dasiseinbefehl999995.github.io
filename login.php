2a23490ea110e1d50acce948a655fad6bbf9928c
puxped <43504306+Enderman842@users.noreply.github.com>
2021-10-28 16:00:03 +0200

    e

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="icon.ico" type="image/icon">
+        <link href="style.css" type="text/css" rel="stylesheet">
+        <title>YEEEEEEEEET</title>
+
+
+    </head>
+    <body>
+        <div id="main">
+            <h1>Login seite.</h1>
+            <p class="twcenmt" style="font-size:18px"></a><a href="index.html">start</a>&nbsp;&nbsp;&nbsp;&nbsp;▪&nbsp;&nbsp;&nbsp;&nbsp;<a href="garbage.html">geschichten</a></p>
+            <hr>
+			<h5>login</h5>
+			
+			
+        </div id="main">
+    </body>
+</html>
